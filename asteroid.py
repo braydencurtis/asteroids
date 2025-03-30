01134c7dbf533a78cb1593e6cf580f98f2478409
Brayden Curtis <brayden@Ovaltine-Jenkins.local>
2025-03-30 14:41:33 -0700

    If asteroid radius is smaller than min constant, just return

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
+
 class Asteroid(CircleShape):
    def __init__(self, x, y, radius):
       super().__init__(x, y, radius)
@@ -13,3 +15,6 @@ class Asteroid(CircleShape):
 
    def split(self):
       self.kill()
+      if self.radius < ASTEROID_MIN_RADIUS:
+         return
+      
