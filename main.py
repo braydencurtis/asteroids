cfcb43737a36a771d79467b3fc1150387a0a5784
Brayden Curtis <brayden@Ovaltine-Jenkins.local>
2025-03-30 13:00:30 -0700

    add comments

--- main.py
+++ main.py
@@ -4,17 +4,24 @@
 import pygame
 import sys
 
+#import the classes we need
 from asteroid import *
 from constants import *
 from player import *
 from asteroidfield import *
 
 def main():
+    # Initialize pygame
     pygame.init()
+    
+    # Set up the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Sett the frame rate and get dt
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create sprite groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
@@ -26,24 +33,32 @@ def main():
     asteroid_field = AsteroidField()
     
 
-
+    # Create the player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        # Update all ubdatable game objects
         updatable.update(dt)
+        
+        # Check for collisions between the player and asteroids
         for asteroid in asteroids:
             if asteroid.collision_detect(player):
                 print("Game Over!")
                 sys.exit()
+        
+        # Draw all drawable game objects
         screen.fill("black")
         for entity in drawable:
             entity.draw(screen)  # Assuming each entity has a draw method that takes the screen
         pygame.display.flip()
 
+        # Limit the frame rate
+        # and get dt
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
